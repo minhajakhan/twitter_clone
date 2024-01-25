6c47a93a1a63b1ace0d8b4de2e1da6a1636f2b3c
minhajakhan <minhajiialamkhan@gmail.com>
2024-01-24 19:36:08 -0500

    user.js added with array of users AND JWT tokens

---
+++ src/users.js
@@ -0,0 +1,39 @@
+const users = [
+  {
+    id: 'iamdillion',
+    name: 'Dillion',
+    image: 'https://dillionmegida.com/img/deee.jpg',
+    bio: 'Just here, doing my thing. Developer advocate at @getstream_io',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWFtZGlsbGlvbiJ9.VO4Rc7MU7pSReBZKNqvMLaPBCFaZcVKI15fdXxSH0LE',
+  },
+  {
+    id: 'getstream_io',
+    name: 'Stream',
+    image: 'https://avatars.githubusercontent.com/u/8597527?s=200&v=4',
+    bio: 'Deploy activity feeds and chat at scale with Stream – an API driven platform powering over a billion end users. Get started at http://getstream.io.',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2V0c3RyZWFtX2lvIn0.on-N7WVaLUwmSVvLKSEa-HK4dvMlAQLR3wI7TywoadE',
+  },
+  {
+    id: 'jake',
+    name: 'Jake',
+    image: 'https://picsum.photos/300/300',
+    bio: 'Just Jake, nothing much',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFrZSJ9.APLrAJdVlxWS_IEbmcro4Swhm9pIrL6dNsVznRfclFg',
+  },
+  {
+    id: 'joe',
+    name: 'Joe',
+    image: 'https://picsum.photos/200/200',
+    bio: 'How are you?',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9lIn0.nKoQlU0SpHmEhGK9dW1B1_1U1wR5BpOjiXVzUC7sS80',
+  },
+  {
+    id: 'mike',
+    name: 'Mike',
+    image: 'https://picsum.photos/400/400',
+    bio: 'I am mike here. I do things on #react and #javascript',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlrZSJ9.cpIBibRw8rQuGOeCFfuvrDILT21NaNV1obyOdLGQONo',
+  },
+]
+
+export default users
