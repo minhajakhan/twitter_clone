755c45b29afd8c67d8009125c885dc816145543a
minhajakhan <minhajiialamkhan@gmail.com>
2024-01-24 20:16:31 -0500

    click a user from the start page to navigate to home route; the selected user is connected to Stream

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
+import StartPage from './pages/StartPage'
+
+import { useEffect, useState } from 'react'
+import { StreamClient } from 'getstream'
+import { StreamApp } from 'react-activity-feed'
+import users from './users'
+import { getFromStorage } from './utils/storage'
+
+const APP_ID = '1280957'
+const API_KEY = 'gvpga2weddxc'
+
+export default function App() {
+  const userId = getFromStorage('user')
+
+  const user = users.find((u) => u.id === userId) || users[0]
+
+  const [client, setClient] = useState(null)
+
+  useEffect(() => {
+    async function init() {
+      const client = new StreamClient(API_KEY, user.token, APP_ID)
+
+      await client.user(user.id).getOrCreate({ ...user, token: '' })
+
+      setClient(client)
+    }
+
+    init()
+  }, [])
+
+  if (!client) return <></>
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StreamApp token={user.token} appId={APP_ID} apiKey={API_KEY}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+        </Routes>
+      </Router>
+    </StreamApp>
+  )
 }
-
-export default App;
